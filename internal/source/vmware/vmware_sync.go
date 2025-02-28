4c098e3dd55606d7ab5bd803743f2bfda12803cf
Gašper Oblak <gasper.oblak@src.si>
2025-02-28 13:42:55 +0100

    fix(vmware): add vlan prefix to missing vlans
    This applies only to vlans that don't already start with vlan prefix

--- internal/source/vmware/vmware_sync.go
+++ internal/source/vmware/vmware_sync.go
@@ -78,6 +78,10 @@ func (vc *VmwareSource) syncNetworks(nbi *inventory.NetboxInventory) error {
 			return fmt.Errorf("vlanTenant: %s", err)
 		}
 		if len(dvpg.VlanIDs) == 1 && len(dvpg.VlanIDRanges) == 0 && dvpg.VlanIDs[0] != 0 {
+			vlanName := dvpg.Name
+			if !strings.HasPrefix(vlanName, "VLAN") {
+				vlanName = fmt.Sprintf("VLAN%04d_%s", dvpg.VlanIDs[0], vlanName)
+			}
 			networkTags := vc.Object2NBTags[dvpgID]
 			vlanStruct := &objects.Vlan{
 				NetboxObject: objects.NetboxObject{
@@ -86,7 +90,7 @@ func (vc *VmwareSource) syncNetworks(nbi *inventory.NetboxInventory) error {
 						constants.CustomFieldSourceIDName: dvpgID,
 					},
 				},
-				Name:   dvpg.Name,
+				Name:   vlanName,
 				Group:  vlanGroup,
 				Site:   vlanSite,
 				Vid:    dvpg.VlanIDs[0],
@@ -580,6 +584,9 @@ func (vc *VmwareSource) collectHostPhysicalNicData(
 				}
 			} else {
 				vlanName := portgroupName
+				if !strings.HasPrefix(vlanName, "VLAN") {
+					vlanName = fmt.Sprintf("VLAN%04d_%s", portgroupData.vlanID, vlanName)
+				}
 				vlanSite, err := common.MatchVlanToSite(vc.Ctx, nbi, vlanName, vc.SourceConfig.VlanSiteRelations)
 				if err != nil {
 					return nil, "", fmt.Errorf("match vlan to site: %s", err)
